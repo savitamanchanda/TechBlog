ace09cabbba1dd1fb40b631e9b7185a68d2b1120
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2023-02-23 09:56:47 -0500

    set up comment model

--- models/index.js
+++ models/index.js
@@ -10,4 +10,22 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog, Comment };
