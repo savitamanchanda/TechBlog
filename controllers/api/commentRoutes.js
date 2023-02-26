f096ba94562816ebd8f6c9e92face40cafe1104f
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2023-02-25 20:21:26 -0500

    last changes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -36,4 +36,23 @@ router.post('/', withAuth, async (req, res) => {
     }
   });
 
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const commentData = await Comment.destroy({
+        where: {
+          blog_id: req.params.id,
+        },
+      });
+  
+      if (!commentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
   module.exports = router;
